159ee506853487feadbbe9edec08bcefacc6cafe
Tarun Bansal <bansaltarun2004@gmail.com>
2025-12-22 21:52:48 +0530

    add subscriptions api

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .models import Subscription
+from .serializers import SubscriptionSerializer
+from .permissions import isOwner
 
 # Create your views here.
+class SubscriptionViewSet(ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = [IsAuthenticated , isOwner]
+
+    # Override get_queryset to return only subscriptions of the auth user
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+    # Override perform_destroy to implement soft delete
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
