aa79f298cadd81d5ecb5b5ec283af3cd1f93736b
Tarun Bansal <bansaltarun2004@gmail.com>
2025-12-23 23:59:24 +0530

    bug fixes

---
+++ subscriptions/urls.py
@@ -0,0 +1,7 @@
+from .views import SubscriptionViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'subscriptions' ,  SubscriptionViewSet , basename='subscriptions')
+
+urlpatterns = router.urls
